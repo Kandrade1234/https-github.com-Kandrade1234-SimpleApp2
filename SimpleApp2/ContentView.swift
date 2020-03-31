b6b3895c963bfc09e9934c94a94a08dcccbd454c
Kevin Andrade <kandrad2@binghamton.edu>
2020-03-30 21:33:18 -0400

    c

--- SimpleApp2/ContentView.swift
+++ SimpleApp2/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         //Text("Life is pain and this is stoopid :( and ELR keeps messing the branches up");
     }
 }
