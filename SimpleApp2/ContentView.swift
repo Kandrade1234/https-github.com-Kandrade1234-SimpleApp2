6b30d7e93cdc1a9e0c9d4c8e5b747551b3035216
Kevin Andrade <kandrad2@binghamton.edu>
2020-03-30 21:23:52 -0400

    changes to text

--- SimpleApp2/ContentView.swift
+++ SimpleApp2/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Life is pain and this is stoopid :(")
+        
+        Text("Life is pain and this is stoopid :( and ELR keeps messing the branches up");
     }
 }
 
