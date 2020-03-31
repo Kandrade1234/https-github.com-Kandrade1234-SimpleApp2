85565660bbf806ccbb16aafb36fcbb664cca80aa
Kevin Andrade <kandrad2@binghamton.edu>
2020-03-30 20:28:17 -0400

    Initial Commit

---
+++ SimpleApp2Tests/SimpleApp2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleApp2Tests.swift
+//  SimpleApp2Tests
+//
+//  Created by Kevin Andrade on 3/30/20.
+//  Copyright © 2020 Kevin Andrade. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleApp2
+
+class SimpleApp2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
